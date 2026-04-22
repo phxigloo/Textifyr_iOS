b78899ba331e674f5ed69b6ddfcb8c59b8dfd047
Bill Miles <phxigloo@gmail.com>
2026-04-22 10:52:33 -0700

    Initial Commit

---
+++ Textifyr_iOS/Textifyr_iOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  Textifyr_iOSApp.swift
+//  Textifyr_iOS
+//
+//  Created by Bill Miles on 4/22/26.
+//
+
+import SwiftUI
+
+@main
+struct Textifyr_iOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Textifyr_iOSTests/Textifyr_iOSTests.swift
@@ -0,0 +1,17 @@
+//
+//  Textifyr_iOSTests.swift
+//  Textifyr_iOSTests
+//
+//  Created by Bill Miles on 4/22/26.
+//
+
+import Testing
+@testable import Textifyr_iOS
+
+struct Textifyr_iOSTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
